afc9367198fd99eeef0be09bbac713b9c04dfeff
Ege Sabanci <egesabanci@outlook.com.tr>
2022-02-05 17:09:41 +0300

    updated docker hub link in comments - 05/02/2022

--- run-node-container.sh
+++ run-node-container.sh
@@ -1,6 +1,6 @@
 # run the ExpressJS backend on 0.0.0.0:8080
 # backend image can be found on Docker Hub
-# (https://hub.docker.com/repository/docker/egesabanci/worqcompany-case-api)
+# (https://hub.docker.com/r/egesabanci/worqcompany-case-api)
 sudo docker run \
   --name api \
   -p 8080:8080 \
